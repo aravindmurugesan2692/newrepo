b7355c198259b12842dfb4c99eb7e7ef0bca8ad6
aravindmurugesan2692 <aravindmurugesan2692@gmail.com>
2023-07-01 02:54:24 +0530

    First commit

---
+++ apple/src/apple/apple.java
@@ -0,0 +1,10 @@
+package apple;
+
+public class apple {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
---
+++ apple/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ArvindM
+ *
+ */
+module apple {
+}
